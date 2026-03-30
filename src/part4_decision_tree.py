0d70fd7b5cc7ec32564d6ebe02a3fb306c3564c9
Alexander Yang <33363583+alexyang430@users.noreply.github.com>
2026-03-29 23:54:27 -0400

    Update part4_decision_tree.py

--- src/part4_decision_tree.py
+++ src/part4_decision_tree.py
@@ -16,3 +16,35 @@ import numpy as np
 from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.model_selection import StratifiedKFold as KFold_strat
 from sklearn.tree import DecisionTreeClassifier as DTC
+
+def run_decision_tree(df_train, df_test):
+
+    features = ['current_charge_felony', 'num_fel_arrests_last_year']
+
+    X_train = df_train[features].fillna(0)
+    y_train = df_train['y']
+
+    X_test = df_test[features].fillna(0)
+
+    param_grid_dt = {'max_depth': [2, 5, 10]}
+
+    dt_model = DecisionTreeClassifier(random_state=42)
+
+    gs_cv_dt = GridSearchCV(dt_model, param_grid_dt, cv=5)
+    gs_cv_dt.fit(X_train, y_train)
+
+    best_depth = gs_cv_dt.best_params_['max_depth']
+    print(f"Best max_depth: {best_depth}")
+
+    if best_depth == min(param_grid_dt['max_depth']):
+        print("Most regularization")
+    elif best_depth == max(param_grid_dt['max_depth']):
+        print("Least regularization")
+    else:
+        print("Middle regularization")
+
+    df_test['pred_dt'] = gs_cv_dt.predict_proba(X_test)[:, 1]
+
+    df_test.to_csv("data/test_final.csv", index=False)
+
+    return df_test

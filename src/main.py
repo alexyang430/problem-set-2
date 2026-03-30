10056a211059b4ddd27fc08be2837e09f2747af6
Alexander Yang <33363583+alexyang430@users.noreply.github.com>
2026-03-29 23:54:50 -0400

    Update main.py

--- src/main.py
+++ src/main.py
@@ -14,14 +14,15 @@ import calibration_plot
 def main():
 
     # PART 1: Instanciate etl, saving the two datasets in `./data/`
-
+    etl.run_etl()
     # PART 2: Call functions/instanciate objects from preprocessing
-
+    df_arrests = preprocessing.run_preprocessing()
     # PART 3: Call functions/instanciate objects from logistic_regression
-
+    df_train, df_test = logistic_regression.run_logistic_regression(df_arrests)
     # PART 4: Call functions/instanciate objects from decision_tree
-
+    df_test = decision_tree.run_decision_tree(df_train, df_test)
     # PART 5: Call functions/instanciate objects from calibration_plot
+    calibration_plot.run_calibration(df_test)
 
 
 if __name__ == "__main__":
